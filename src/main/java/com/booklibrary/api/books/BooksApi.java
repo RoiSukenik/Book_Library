84e7ce740a98e379029218ff38e24de214e9eae1
RoiSukenik <roisukenik@gmail.com>
2022-05-12 23:21:55 +0300

    Fixes to annotation and controllers api

--- src/main/java/com/booklibrary/api/books/BooksApi.java
+++ src/main/java/com/booklibrary/api/books/BooksApi.java
@@ -3,18 +3,19 @@ package com.booklibrary.api.books;
 import com.booklibrary.api.books.bookdtos.BookDto;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public interface BooksApi {
 
     String BASE_PATH = "books";
 
-    @GetMapping(value = "/", name = "Get a book ")
-    BookDto getBookByName(String bookName);
+    @GetMapping(value = "/{bookName}", name = "Get a book ")
+    BookDto getBookByName(@PathVariable("bookName") String bookName);
 
     @PostMapping(value = "/", name = "Add a book")
     String addBook(BookDto newBook);
 
-    @DeleteMapping(value = "/{id}",name = "Delete a book")
-    void deleteBook(String bookName);
+    @DeleteMapping(value = "/{bookName}", name = "Delete a book")
+    void deleteBook(@PathVariable("bookName") String bookName);
 }
